8931ca730c7dc24f6ab176466ef9a1412ced4712
Suho Park <psooh1219@icloud.com>
2022-11-29 19:43:38 +0900

    Day 17. Creating pickers in a form

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -14,19 +14,33 @@ struct ContentView: View {
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            // $checkAmount를 text에 바로 입력하는 것은 String 형식과 일치하지 않아서 불가
-            // value: , format: init으로 수행 가능
-            // "USD를 사용하지 않는 사람이 많음
-            // Locale: 사용자의 모든 지역 설정을 저장하는 iOS의 구조체
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad) // 키보드를 강제하는 법
+        // NavigationView
+        //  1. 상단에 제목을 배치할 수 있는 공강 제공
+        //  2. iOS가 새 뷰에서 슬라이드 할 수 있도록 함
+        NavigationView {
+            Form {
+                // $checkAmount를 text에 바로 입력하는 것은 String 형식과 일치하지 않아서 불가
+                // value: , format: init으로 수행 가능
+                // "USD를 사용하지 않는 사람이 많음
+                // Locale: 사용자의 모든 지역 설정을 저장하는 iOS의 구조체
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad) // 키보드를 강제하는 법
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
-            
-            Section {
-                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            }
+            .navigationTitle("WeSplit")
+            // Form의 수정자로 사용하는 이유: NavigationView가 많은 View를 표시할 수 있기 때문
+            // 내부의 항목에 제목을 첨부하면 iOS에서 제목을 자유롭게 변경할 수 있음
         }
     }
 }
