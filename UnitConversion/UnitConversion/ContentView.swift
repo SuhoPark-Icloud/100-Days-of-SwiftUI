6e6fb89b1b32b33c0bf2edb50ad10593c1d0e264
Suho Park <psooh1219@icloud.com>
2022-12-02 22:03:45 +0900

    Day 19. Done UnitConversion project!

--- UnitConversion/UnitConversion/ContentView.swift
+++ UnitConversion/UnitConversion/ContentView.swift
@@ -8,18 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var inputValue: Double?
-    @State private var inputUnit = "m"
-    @State private var outputUnit = "km"
-    @FocusState private var inputValueFocused: Bool
-    var lengthUnits = ["m", "km", "ft", "yd", "mi"]
+    var units = ["m", "km", "ft", "yd", "mi"]
     
+    @State private var inputUnit: String = "m"
+    @State private var outputUnit = "km"
+    @FocusState private var inputLengthFocused: Bool
+    
+    @State private var inputLength = 1.0
+    
+    var inputLength_m: Double {
+        switch inputUnit {
+        case "m":
+            return inputLength
+        case "km":
+            return inputLength * 1000
+        case "ft":
+            return inputLength * 0.3048
+        case "yd":
+            return inputLength * 0.9144
+        case "mi":
+            return inputLength * 1609.34
+        default:
+            return 0.0
+        }
+    }
+    
+    var outputLength: Double {
+        switch outputUnit {
+        case "m":
+            return inputLength_m
+        case "km":
+            return inputLength_m / 1000
+        case "ft":
+            return inputLength_m / 0.3048
+        case "yd":
+            return inputLength_m / 0.9144
+        case "mi":
+            return inputLength_m / 1609.34
+        default:
+            return 0.0
+        }
+    }
     
     var body: some View {
         Form {
             Section {
                 Picker("Input units", selection: $inputUnit) {
-                    ForEach(lengthUnits, id: \.self) {
+                    ForEach(units, id: \.self) {
                         Text($0)
                     }
                 }
@@ -29,28 +64,28 @@ struct ContentView: View {
             }
             
             Section {
-                Picker("Input units", selection: $outputUnit) {
-                    ForEach(lengthUnits, id: \.self) {
+                Picker("Output units", selection: $outputUnit) {
+                    ForEach(units, id: \.self) {
                         Text($0)
                     }
                 }
                 .pickerStyle(.segmented)
             } header: {
-                Text("input units")
+                Text("Output units")
             }
             
             Section {
-                TextField("Put the value", value: $inputValue, format: .number)
+                TextField("Please enter length", value: $inputLength, format: .number)
                     .keyboardType(.decimalPad)
-                    .focused($inputValueFocused)
+                    .focused($inputLengthFocused)
             } header: {
-                Text("input value")
+                Text("input length")
             }
             
             Section {
-                Text("100")
+                Text(outputLength, format: .number)
             } header: {
-                Text("output value")
+                Text("output length")
             }
         }
         .toolbar {
@@ -58,7 +93,7 @@ struct ContentView: View {
                 Spacer()
                 
                 Button("Done") {
-                    inputValueFocused = false
+                    inputLengthFocused = false
                 }
             }
         }
