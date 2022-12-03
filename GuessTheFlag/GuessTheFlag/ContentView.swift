f9b7bc913c0ab1f68bee61aa3fbbbf158cfdf830
Suho Park <psooh1219@icloud.com>
2022-12-03 14:54:52 +0900

    Day 20. Colors and frames 2

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,18 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            // Color 그 자체도 View
-            Color.red
-//                .frame(width: 200, height: 200)
-                .frame(minWidth: 200, maxWidth: .infinity, minHeight: 200)
-            // 의미론적인 색상
-            Color.primary //: 검은색/흰색(일반모드/다크모드)
-            Color.secondary // 어두운회색/밝은회색(일반모드/다크모드)
-            
-            // RGB 색상
-            Color(red: 1, green: 0.8, blue: 0)
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
             
             Text("Your content")
+//                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
         // 안전영역 아래로 채우기
         .ignoresSafeArea()
