117280f725e8f71818e940d12138f23982f51e55
Suho Park <psooh1219@icloud.com>
2022-12-03 15:33:51 +0900

    Day 20. Buttons and images

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,27 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        // 선형 그래디언트
-        LinearGradient(gradient: Gradient(stops: [
-//            Gradient.Stop(color: .white, location: 0.45),
-//            Gradient.Stop(color: .black, location: 0.55)
-            // 여기에서 Gradient.Stop을 생성할 것을 알고 있으므로 유형 생략 가능
-            .init(color: .white, location: 0.45),
-            .init(color: .black, location: 0.55)
-        ]), startPoint: .top, endPoint: .bottom)
-            .ignoresSafeArea()
-        
-        // 방사형 그래디언트
-        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
-            .ignoresSafeArea()
-        
-        // 각도 그래디언트
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
-            .ignoresSafeArea()
-        
-        // 모든 그라데이션 유형에는 색상 뿐만 아니라 정지점 제공
-        // 독립 실행도 가능하고, 수정자의 일부로 사용 가능
-        //  예) Text()의 배경
+        VStack {
+            Button("Button 1") {}
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) {}
+                .buttonStyle(.bordered)
+            Button("Button 3") {}
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button("Button 4", role: .destructive) {}
+                .buttonStyle(.borderedProminent)
+            
+            // 사용자 지정 버튼
+            Button {
+                print("Button was tapped")
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.red)
+            }
+            
+            // 이 방식은 이미지(Image)를 단추에 통합하려는 경우에 주로 사용
+            Button {
+                print("Edit button was tapped")
+            } label: {
+//                Image(systemName: "pencil")
+                // Label: 이미지와 텍스트를 동시에 사용하고자 할 때 사용
+                Label("Edit", systemImage: "pencil")
+            }
+
+        }
+    }
+    
+    func executeDelete() {
+        print("Now deleting")
     }
 }
 
