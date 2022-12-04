a46be3952e2edc7be41ae9f93bf04bd7ff96bfa8
Suho Park <psooh1219@icloud.com>
2022-12-04 11:51:04 +0900

    Day 21. Stacking up buttons

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,22 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
-        }
-        // 수정자가 사용되는 위치는 중요하지 X
-        // 모든 작업은 경고가 존재하고, showingAlert가 true일 때 경고 표시
-        .alert("Important message", isPresented: $showingAlert) {
-            // alert 내부 모든 버튼은 자동으로 alert 해제
-//            Button("Delete", role: .destructive) {}
-//            Button("Cancel", role: .cancel) {}
-            Button("OK", role: .cancel) {}
-        } message: {
-            // 두 번째 후행 클로저를 사용하여 제목 아래 메시지 추가 가능
-            Text("Please read this.")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
         }
     }
 }
