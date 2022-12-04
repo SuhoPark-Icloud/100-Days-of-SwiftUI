7d56c12ec464b997d055d42320d5f9a8319cc4de
Suho Park <psooh1219@icloud.com>
2022-12-04 13:13:48 +0900

    Day 21. Remove unnecessary comments

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -17,7 +17,6 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            //            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
