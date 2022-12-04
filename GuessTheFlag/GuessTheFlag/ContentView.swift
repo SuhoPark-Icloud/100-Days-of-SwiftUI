3d6a2ea2b795be3bb3977ca1eee010e7bf14dfc6
Suho Park <psooh1219@icloud.com>
2022-12-04 13:13:10 +0900

    Day 21. Upgrading our design

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -17,34 +17,62 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
+            //            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                VStack {
-                    // .weight(): 요청하는 글꼴에 수정자를 추가하여 세밀하게 제어 가능
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    
-                    // .largeTitle: iOS에서 제공하는 가장 큰 내장 글꼴
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                    
-                }
+            
+            VStack {
+                // 크기가 큰 화면에서 각 항목들이 너무 벌어지지 않게끔 하는 효과
+                // 실제로 크기가 작은 화면에서는 그 효과가 미비하게 나타날 수 있다.
+                Spacer()
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original) // 원래 이미지 픽셀 그대로 렌더링
-                            .clipShape(Capsule()) // 캡슐 이미지 형태로 수정
-                            .shadow(color: .primary, radius: 5) // 그림자 설정
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        // .weight(): 요청하는 글꼴에 수정자를 추가하여 세밀하게 제어 가능
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        // .largeTitle: iOS에서 제공하는 가장 큰 내장 글꼴
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                        
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original) // 원래 이미지 픽셀 그대로 렌더링
+                                .clipShape(Capsule()) // 캡슐 이미지 형태로 수정
+                                .shadow(radius: 5) // 그림자 설정
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: ???")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
