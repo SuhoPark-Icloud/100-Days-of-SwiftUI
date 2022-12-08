384b0491ffd6613020ef3597a62339129f754997
Suho Park <psooh1219@icloud.com>
2022-12-08 18:16:59 +0900

    Day 23. Custom modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,55 +7,30 @@
 
 import SwiftUI
 
-// ViewModifier를 활용하여 새로운 View를 만들어 낼 수도 있다.
-struct Watermakr: ViewModifier {
-    var text: String
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
     
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(.black)
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
         }
-    }
-}
-
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
-extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
-    }
-    
-    func watermarked(with text: String) -> some View {
-        modifier(Watermakr(text: text))
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-//            .modifier(Title())
-        // ViewModifier로 구성한 객체를 생성하여 수정자 적용 가능
-            .titleStyle()
-        // extension을 활용하여 기존의 다른 수정자처럼 구성 가능
-        
-        Color.blue
-            .frame(width: 300, height: 200)
-            .watermarked(with: "Hacking with Swift")
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {
