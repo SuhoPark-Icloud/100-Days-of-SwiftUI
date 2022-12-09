b2e0111d262eedb13fb43b7a9c31f63e31229176
Suho Park <psooh1219@icloud.com>
2022-12-09 17:39:01 +0900

    Day 25. Custom binding

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,29 +7,26 @@
 
 import SwiftUI
 
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    @ViewBuilder let content: (Int, Int) -> Content
-    
-    var body: some View {
-        VStack {
-            ForEach(0..<rows, id: \.self) { row in
-                HStack {
-                    ForEach(0..<columns, id: \.self) { column in
-                        content(row, column)
-                    }
-                }
-            }
-        }
-    }
-}
 
 struct ContentView: View {
+    @State private var agreedToTerms = false
+    @State private var agreedToPrivacyPolicy = false
+    @State private var agreedToEmails = false
+    
     var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col in
-            Image(systemName: "\(row * 4 + col).circle")
-            Text("R\(row) C\(col)")
+        let agreedToAll = Binding<Bool>(get: {
+            agreedToTerms && agreedToPrivacyPolicy && agreedToEmails
+        }, set: {
+            agreedToTerms = $0
+            agreedToPrivacyPolicy = $0
+            agreedToEmails = $0
+        })
+        
+        return VStack {
+            Toggle("Agree to terms", isOn: $agreedToTerms)
+            Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
+            Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
+            Toggle("Agree to all", isOn: agreedToAll)
         }
     }
 }
