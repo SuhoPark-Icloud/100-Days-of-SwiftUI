09b85256dd019ae38f275d67d2381f27b810dfa9
Suho Park <psooh1219@icloud.com>
2022-12-10 21:03:07 +0900

    Day 26. Working with dates

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
-    
     var body: some View {
-        DatePicker("", selection: $wakeUp, in: Date.now...)
-            .labelsHidden()
+        // 국가에 따라 년월일을 정렬하는 순서가 달라질 수 있기 때문에
+        // 이를 formatted에 맡길 수 있다.
+        Text(Date.now.formatted(date: .long, time: .omitted))
+    }
+    
+    func trivialExample() {
+        var components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
