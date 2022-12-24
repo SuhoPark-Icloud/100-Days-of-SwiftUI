50307f1229d80b7ff82cfb9dc9c4ce646282bf63
Suho Park <psooh1219@icloud.com>
2022-12-24 21:57:17 +0900

    Day 30. [WordScramble] Implementing this game

--- project5/WordScramble/WordScramble/ContentView.swift
+++ project5/WordScramble/WordScramble/ContentView.swift
@@ -8,19 +8,116 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWord = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                    // 입력한 문자열의 첫번째 글자가 대문자가 되지 않게 하기 위함
+                }
+                
+                Section {
+                    ForEach(usedWord, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
+        }
     }
     
-    func test() {
-        let word = "swift"
-        let checker = UITextChecker()
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
         
+        guard isOrigianl(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        // 애니메이션 적용
+        withAnimation {
+            // 0으로 설정하여 자동으로 목록 상단에 표시
+            usedWord.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could nor load start.txt from bundle.")
+    }
+    
+    // 기존 추가한 단어인지 확인
+    func isOrigianl(word: String) -> Bool {
+        !usedWord.contains(word)
+    }
+    
+    // rootWord에서 제시한 알파벳과 동일한지 확인
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    // 사전에 포함되어 있는 단어인지 확인
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
-        // Objective-C에는 옵셔널의 개념이 없었기 때문에 특수한 값인 NSNotFound을 통해 철자에 문제가 없는지 확인할 수 있음
-        let allGood = misspelledRange.location == NSNotFound
+        return misspelledRange.location == NSNotFound
+    }
+    
+    // 에러 제목과 내용을 채워 alert 창을 띄우기 위해 사용
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
