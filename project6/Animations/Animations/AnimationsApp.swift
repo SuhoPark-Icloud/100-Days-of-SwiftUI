a4a80e2dd439a3b04bd67b40dff31477a86a6d2f
Suho Park <psooh1219@icloud.com>
2022-12-27 20:45:35 +0900

    Day 32. [Animations] Customizing animations in SwiftUI

---
+++ project6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by 박수호 on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
