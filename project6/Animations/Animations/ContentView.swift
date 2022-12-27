ed1e4879b3abcd35b51e21ffb17c09e7f719b88e
Suho Park <psooh1219@icloud.com>
2022-12-27 21:02:08 +0900

    Day 32. [Animations] Animating bindings

--- project6/Animations/Animations/ContentView.swift
+++ project6/Animations/Animations/ContentView.swift
@@ -11,26 +11,26 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            // animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
+        VStack {
+            // Binding animation
+            // View에 애니메이션을 설정하는 것이 아닌 바인딩된 값에 애니메이션 적용
+            Stepper("Scale amount", value: $animationAmount.animation(
+                // 특수 효과도 삽입 가능
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            // 버튼에는 .animation()을 적용하지 않았기 때문에 버튼을 눌렀을 때에는 애니메이션 적용 X
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
     }
 }
