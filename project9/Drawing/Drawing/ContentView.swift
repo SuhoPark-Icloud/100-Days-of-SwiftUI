e02e7373aafdb950617a582deebabbae2696b6c8
Suho Park <psooh1219@icloud.com>
2023-03-16 20:05:17 +0900

    Day 44. [Drawing] drawingGroup()으로 고성능 Metal 렌더링 활성화

--- project9/Drawing/Drawing/ContentView.swift
+++ project9/Drawing/Drawing/ContentView.swift
@@ -7,16 +7,54 @@
 
 import SwiftUI
 
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        // 이것은 SwiftUI가 훨씬 빠른 단일 렌더링 출력으로 화면에 다시 넣기 전에 뷰의 내용을 오프스크린 이미지로 렌더링해야 한다고 알려줌
+        // 성능을 위해 매우 좋은 수정자이나, 남발하면 SwiftUI 자체의 성능이 나빠질 수 있음
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
+    @State private var colorCycle = 0.0
+    
     var body: some View {
         VStack {
-            Text("Hello World")
+            ColorCyclingCircle(amount: colorCycle)
                 .frame(width: 300, height: 300)
-                .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
             
-            Capsule()
-                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
-                .frame(width: 300, height: 200)
+            Slider(value: $colorCycle)
+            
         }
     }
 }
