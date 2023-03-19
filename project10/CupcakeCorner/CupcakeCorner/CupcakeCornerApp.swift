18a31723081025ffcfd21f7a4dd4a0a199561b3d
Suho Park <psooh1219@icloud.com>
2023-03-19 16:32:34 +0900

    Day 49. [CupcakeCorner] @Published 속성에 대한 Codable 적합성 추가

---
+++ project10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by 박수호 on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
