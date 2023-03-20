d24f49ec546e9fc62cde1fef8b4bbe1610ae0835
Suho Park <psooh1219@icloud.com>
2023-03-20 19:23:02 +0900

    Day 50. [CupcakeCorner] 본격 프로젝트 시작 전 기존 파일 정리

---
+++ project10/CupcakeCorner/CupcakeCorner/Practices/TaylorSwiftSongsView.swift
@@ -0,0 +1,60 @@
+//
+//  TaylorSwiftSongsView.swift
+//  CupcakeCorner
+//
+//  Created by 박수호 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct TaylorSwiftSongsView: View {
+    @State private var results = [Result]()
+
+    var body: some View {
+        VStack {
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    Text(item.collectionName)
+                }
+            }
+            .task {
+                await loadData()
+            }
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+    }
+}
+
+struct TaylorSwiftSongsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaylorSwiftSongsView()
+    }
+}
