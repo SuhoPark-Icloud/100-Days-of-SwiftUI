f3b7449cb89600a792c7b76b01dc199947365f26
Suho Park <psooh1219@icloud.com>
2023-03-20 20:16:54 +0900

    Day 50. [CupcakeCorner] 어플을 구성하는 기본적인 View들의 구성 및 Order 클래스의 데이터 참조 구성

--- project10/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ project10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var order = Order()
+    
     var body: some View {
-        Text("Hello")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        // indices: 배열의 인덱스 목록
+                        // warning을 제거하기 위해 id: \.self를 추가
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(
+                        "Number of cakes: \(order.quantity)",
+                        value: $order.quantity,
+                        in: 3...20
+                    )
+                }
+                
+                Section {
+                    Toggle(
+                        "Any special requests?",
+                        isOn: $order.specialRequestEnabled.animation()
+                    )
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
+
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
     }
 }
 
