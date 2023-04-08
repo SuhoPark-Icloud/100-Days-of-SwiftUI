339402de1a254272f02c6e34e8b4211038429a9b
Suho Park <psooh1219@icloud.com>
2023-04-09 00:47:43 +0900

    Day 52. Bookworm project 생성

---
+++ project11/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by 박수호 on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
