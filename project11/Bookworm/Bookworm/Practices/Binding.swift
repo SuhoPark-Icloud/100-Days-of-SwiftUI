f06e55b2cc28038d8601ef42d0b67c21d9114662
Suho Park <psooh1219@icloud.com>
2023-04-09 21:04:04 +0900

    @Binding property wrapper 적용 (그러나 struct에서 구현 안됨..)

---
+++ project11/Bookworm/Bookworm/Practices/Binding.swift
@@ -0,0 +1,47 @@
+//
+//  Binding.swift
+//  Bookworm
+//
+//  Created by 박수호 on 2023/04/09.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    let title: String
+    @State var isOn: Bool
+//    @Binding var isOn: Bool // 안되는 이유는 잘 모르겠음...
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(
+            LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom)
+        )
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct Binding: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+struct Binding_Previews: PreviewProvider {
+    static var previews: some View {
+        Binding()
+    }
+}
