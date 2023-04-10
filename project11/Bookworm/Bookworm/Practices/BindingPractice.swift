5e00cb33d95939fe51a6eb5822639ac67598eeab
Suho Park <psooh1219@icloud.com>
2023-04-10 18:20:20 +0900

    Day 54. [Bookworm] 커스텀 별 평점 view 생성

--- project11/Bookworm/Bookworm/Practices/BindingPractice.swift
+++ project11/Bookworm/Bookworm/Practices/BindingPractice.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct PushButton: View {
     let title: String
-    @State var isOn: Bool
-//    @Binding var isOn: Bool // 안되는 이유는 잘 모르겠음...
+    @Binding var isOn: Bool // 안되는 이유는 잘 모르겠음...
     
     var onColors = [Color.red, Color.yellow]
     var offColors = [Color(white: 0.6), Color(white: 0.4)]
@@ -29,12 +28,12 @@ struct PushButton: View {
     }
 }
 
-struct Binding: View {
+struct BindingPractice: View {
     @State private var rememberMe = false
     
     var body: some View {
         VStack {
-            PushButton(title: "Remember Me", isOn: rememberMe)
+            PushButton(title: "Remember Me", isOn: $rememberMe)
             Text(rememberMe ? "On" : "Off")
         }
     }
@@ -42,6 +41,6 @@ struct Binding: View {
 
 struct Binding_Previews: PreviewProvider {
     static var previews: some View {
-        Binding()
+        BindingPractice()
     }
 }
