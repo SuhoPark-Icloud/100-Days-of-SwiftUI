bd154f648928f43acb49a6106f5b3c3009429f5f
Suho Park <psooh1219@icloud.com>
2023-04-16 21:17:36 +0900

    Day 58. [CoreData] SwiftUI로 @FetchRequest를 동적으로 필터링

---
+++ project12/CoreDataProject/CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by 박수호 on 2023/04/16.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
