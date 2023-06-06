3756b6579a232743c904a903f12f9006efd55b53
Suho Park <psooh1219@icloud.com>
2023-06-06 14:15:56 +0900

    Day 62. [Instafilter] 프로젝트 생성

---
+++ project13/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by 박수호 on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
