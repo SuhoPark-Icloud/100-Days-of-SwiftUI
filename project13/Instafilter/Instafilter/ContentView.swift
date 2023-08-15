e1ab87157ccb0071054d794ad0d84f74dd10ad15
Suho Park <psooh1219@icloud.com>
2023-08-15 16:44:21 +0900

    Day 62. [Instafilter] onChange()를 사용하여 상태 변경에 응답하기

--- project13/Instafilter/Instafilter/ContentView.swift
+++ project13/Instafilter/Instafilter/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount: CGFloat = 0.0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .blur(radius: blurAmount)
+
+            Slider(value: $blurAmount, in: 0 ... 20)
+                .onChange(of: blurAmount) { newValue in
+                    print("New value is \(newValue)")
+                }
         }
-        .padding()
     }
 }
 
