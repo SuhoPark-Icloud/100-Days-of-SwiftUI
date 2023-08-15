e2bba4f95f74abd53aeddb9ddc64790a785f8b58
Suho Park <psooh1219@icloud.com>
2023-08-15 16:48:43 +0900

    Day 62. [Instafilter] confirmationDialog()로 여러 옵션 표시

--- project13/Instafilter/Instafilter/ContentView.swift
+++ project13/Instafilter/Instafilter/ContentView.swift
@@ -8,22 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount: CGFloat = 0.0 {
-        didSet {
-            print("New value is \(blurAmount)")
-        }
-    }
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
 
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .blur(radius: blurAmount)
-
-            Slider(value: $blurAmount, in: 0 ... 20)
-                .onChange(of: blurAmount) { newValue in
-                    print("New value is \(newValue)")
-                }
-        }
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                showingConfirmation = true
+            }
+            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColor = .red }
+                Button("Green") { backgroundColor = .green }
+                Button("Blue") { backgroundColor = .blue }
+                Button("Cancle", role: .cancel) {}
+            } message: {
+                Text("Select a new color")
+            }
     }
 }
 
