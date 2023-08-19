8a1316b48f080f2e2341e2de9507928e515d68bc
Suho Park <psooh1219@icloud.com>
2023-08-19 13:11:53 +0900

    Day 64. [Instafilter] 사용자의 사진 라이브러리에 이미지를 저장하는 방법

---
+++ project13/Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by 박수호 on 2023/08/19.
+//
+
+import SwiftUI
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
