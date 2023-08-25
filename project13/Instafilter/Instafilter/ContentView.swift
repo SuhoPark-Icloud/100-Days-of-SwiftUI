0586e87640098afc50ee92fc77b524f0ecea0266
Suho Park <psooh1219@icloud.com>
2023-08-25 23:44:02 +0900

    Day 66. [Instafilter] UIImageWriteToSavedPhotosAlbum()을 사용하여 필터링된 이미지 저장하기

--- project13/Instafilter/Instafilter/ContentView.swift
+++ project13/Instafilter/Instafilter/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
     @State private var inputImage: UIImage?
     @State private var currentFilter: CIFilter = .sepiaTone()
     @State private var showingFilterSheet = false
+    @State private var processedImage: UIImage?
     let context = CIContext()
 
     var body: some View {
@@ -74,7 +75,18 @@ struct ContentView: View {
         }
     }
 
-    func save() {}
+    func save() {
+        guard let processedImage = processedImage else { return }
+
+        let imageSaver = ImageSaver()
+        imageSaver.successHandler = {
+            print("Success!")
+        }
+        imageSaver.errorHandler = {
+            print("Oops: \($0.localizedDescription)")
+        }
+        imageSaver.writeToPhotoAlbum(image: processedImage)
+    }
 
     func loadImage() {
         guard let inputImage = inputImage else { return }
@@ -96,6 +108,7 @@ struct ContentView: View {
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
     }
 
