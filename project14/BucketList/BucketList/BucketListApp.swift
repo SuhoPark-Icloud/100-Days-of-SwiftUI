c9b34ef54c3c9c247e539c5120b5d8156a5f92c1
Suho Park <psooh1219@icloud.com>
2023-08-26 09:35:20 +0900

    Day 68. [BucketList] 프로젝트 생성

---
+++ project14/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by 박수호 on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
