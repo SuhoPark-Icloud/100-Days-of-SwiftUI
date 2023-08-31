390af96921abd87ed03a13a9f8c4133262485271
Suho Park <psooh1219@icloud.com>
2023-09-01 06:49:36 +0900

    Day 71. [BucketList] 위키피디아 결과 정렬

--- project14/BucketList/BucketList/EditView.swift
+++ project14/BucketList/BucketList/EditView.swift
@@ -37,7 +37,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                                 + Text(": ") +
-                                Text("Page description here")
+                                Text(page.description)
                                 .italic()
                         }
                     case .loading:
@@ -86,7 +86,7 @@ struct EditView: View {
 
             let item = try JSONDecoder().decode(Result.self, from: data)
 
-            pages = item.query.pages.values.sorted { $0.title < $1.title }
+            pages = item.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             loadingState = .failed
