9d1ee638104e7acf84d218b81f9631a9bea9b434
Suho Park <psooh1219@icloud.com>
2023-09-03 08:32:27 +0900

    Day 74. [Accessibility] 프로젝트 생성

---
+++ project15/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by 박수호 on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
