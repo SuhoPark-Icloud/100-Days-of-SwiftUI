75c941da143b5819efa466163c2d8ab7857e1ea1
Suho Park <psooh1219@icloud.com>
2023-09-03 10:15:14 +0900

    Day 74. [Accessibility] 접근성 데이터 숨기기 및 그룹화하기

--- project15/AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ project15/AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,31 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096",
-    ]
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks",
-    ]
-
-    @State private var selectedPicture = Int.random(in: 0 ... 3)
-
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-                selectedPicture = Int.random(in: 0 ... 3)
+        VStack {
+            Image(decorative: "character")
+                .resizable()
+                .scaledToFit()
+                .accessibilityHidden(true)
+
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
             }
-            .accessibilityLabel(labels[selectedPicture])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Your score is 1000")
+        }
     }
 }
 
