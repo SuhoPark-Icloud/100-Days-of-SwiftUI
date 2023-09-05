154412718aed54a5de4c3cf156a74cf9c44c6f8c
Suho Park <psooh1219@icloud.com>
2023-09-05 19:01:40 +0900

    Day 79. [HotProspects] @EnvironmentObject로 환경에서 사용자 지정 값 읽기

---
+++ project16/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by 박수호 on 2023/09/05.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
