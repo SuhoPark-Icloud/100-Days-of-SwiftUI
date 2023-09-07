9ce72fcf8af244f7cc8de5f780d191bff6ffe8e6
Suho Park <psooh1219@icloud.com>
2023-09-07 14:59:28 +0900

    Day 82. [HotProspects] SwiftUI List를 동적으로 필터링하기

--- project16/HotProspects/HotProspects/ProspectsView.swift
+++ project16/HotProspects/HotProspects/ProspectsView.swift
@@ -12,7 +12,10 @@ struct ProspectsView: View {
         case none, contacted, uncontacted
     }
 
+    @EnvironmentObject var prospects: Prospects
+
     let filter: FilterType
+
     var title: String {
         switch filter {
         case .none:
@@ -24,22 +27,40 @@ struct ProspectsView: View {
         }
     }
 
-    @EnvironmentObject var prospects: Prospects
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
 
     var body: some View {
         NavigationView {
-            Text("People: \(prospects.people.count)")
-                .navigationTitle(title)
-                .toolbar {
-                    Button {
-                        let prospect = Prospect()
-                        prospect.name = "Paul Hudson"
-                        prospect.emailAddress = "paul@hackingwithswift.com"
-                        prospects.people.append(prospect)
-                    } label: {
-                        Label("Scan", systemImage: "qrcode.viewfinder")
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
                     }
                 }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button {
+                    let prospect = Prospect()
+                    prospect.name = "Paul Hudson"
+                    prospect.emailAddress = "paul@hackingwithswift.com"
+                    prospects.people.append(prospect)
+                } label: {
+                    Label("Scan", systemImage: "qrcode.viewfinder")
+                }
+            }
         }
     }
 }
