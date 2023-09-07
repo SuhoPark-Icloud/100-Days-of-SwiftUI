ef564fe633ee371d5e4f066a114d5286d0d4a666
Suho Park <psooh1219@icloud.com>
2023-09-07 14:37:59 +0900

    Day 82. [HotProspects] 탭 바 만들기

--- project16/HotProspects/HotProspects/ContentView.swift
+++ project16/HotProspects/HotProspects/ContentView.swift
@@ -5,19 +5,28 @@
 //  Created by 박수호 on 2023/09/05.
 //
 
-import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    let possibleNumbers = Array(1 ... 60)
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ", ")
-    }
-
     var body: some View {
-        Text(results)
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
     }
 }
 
