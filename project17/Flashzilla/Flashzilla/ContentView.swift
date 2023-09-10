379a3fda9d5a054eda3588c7e7079e4a05d84e44
Suho Park <psooh1219@icloud.com>
2023-09-11 08:52:43 +0900

    Day 86. [Flashzilla] UINotificationFeedbackGenerator 및 Core Haptics로 진동 만들기

--- project17/Flashzilla/Flashzilla/ContentView.swift
+++ project17/Flashzilla/Flashzilla/ContentView.swift
@@ -5,44 +5,68 @@
 //  Created by 박수호 on 2023/09/11.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    // 원이 얼마나 멀리 끌려갔는지
-    @State private var offset = CGSize.zero
-
-    // 현재 드래그되고 있는지 여부
-    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
 
     var body: some View {
-        // 오프셋을 업데이트하고 움직일 때 드래그하는 드래그 제스처
-        let dragGesture = DragGesture()
-            .onChanged { value in offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
+        Text("Hello, world!")
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
+    }
 
-        // isDragging을 가능하게 하는 길게 누르는 제스처
-        let pressGesture = LongPressGesture()
-            .onEnded { _ in
-                withAnimation {
-                    isDragging = true
-                }
-            }
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.warning)
+    }
 
-        // 사용자가 길게 누른 다음 드래그하도록 강제하는 결합된 제스처
-        let combined = pressGesture.sequenced(before: dragGesture)
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
 
-        // 드래그할 때 크기를 조정하고, 드래그 제스처에서 가져온 모든 것으로 오프셋을 설정하고, 결합된 제스처를 사용하는 64x64 원
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+
+    func complexSuccess() {
+        // 장치가 햅틱을 지원하는지 확인
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // 강렬하고 날카로운 탭 하나 생성
+//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+//        events.append(event)
+
+        // 강도와 선명도를 증가시키다가 감소시키는 여러 탭 생성
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+
+        // 그 이벤트들을 패턴으로 변환하고 즉시 재생
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
     }
 }
 
