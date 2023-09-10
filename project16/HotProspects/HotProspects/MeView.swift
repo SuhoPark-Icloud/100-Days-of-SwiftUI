4e3908711f237dc65768826890b6502d3927c331
Suho Park <psooh1219@icloud.com>
2023-09-10 19:00:26 +0900

    Day 84. [HotProspects] 이미지에 상황에 맞는 메뉴 추가하기

--- project16/HotProspects/HotProspects/MeView.swift
+++ project16/HotProspects/HotProspects/MeView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
 
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -26,13 +27,24 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode() }
         }
     }
 
@@ -41,11 +53,16 @@ struct MeView: View {
 
         if let outputImage = filter.outputImage {
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgimg)
+                qrCode = UIImage(cgImage: cgimg)
+                return qrCode
             }
         }
 
         return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
 }
 
