77dae773e47503082b70b4e017ba0db9e9163e93
Suho Park <psooh1219@icloud.com>
2023-09-12 08:43:39 +0900

    Day 87. [Flashzilla] SwiftUI로 특정 접근성 요구 사항 지원 - 모션 줄이기

--- project17/Flashzilla/Flashzilla/ContentView.swift
+++ project17/Flashzilla/Flashzilla/ContentView.swift
@@ -8,20 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @State private var scale = 1.0
 
     var body: some View {
-        HStack {
-            if differentiateWithoutColor {
-                Image(systemName: "checkmark.circle")
+        Text("Hello, World!")
+            .scaleEffect(scale)
+            .onTapGesture {
+                withOptionalAnimation {
+                    scale *= 1.5
+                }
             }
+    }
 
-            Text("Success")
+    // 모션 줄이기 옵션에 따라 자동 적용
+    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+        if UIAccessibility.isReduceMotionEnabled {
+            return try body()
+        } else {
+            return try withAnimation(animation, body)
         }
-        .padding()
-        .background(differentiateWithoutColor ? .black : .green)
-        .foregroundColor(.white)
-        .clipShape(Capsule())
     }
 }
 
