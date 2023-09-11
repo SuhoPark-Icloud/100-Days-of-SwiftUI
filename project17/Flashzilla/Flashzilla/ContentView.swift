58bc2b106a47f62e0e74ecd681434f13ecccc042
Suho Park <psooh1219@icloud.com>
2023-09-11 13:54:15 +0900

    Day 86. [Flashzilla] allowsHitTesting()으로 사용자 상호 작용 비활성화하기

--- project17/Flashzilla/Flashzilla/ContentView.swift
+++ project17/Flashzilla/Flashzilla/ContentView.swift
@@ -5,67 +5,37 @@
 //  Created by 박수호 on 2023/09/11.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
-
     var body: some View {
-        Text("Hello, world!")
-            .onAppear(perform: prepareHaptics)
-            .onTapGesture(perform: complexSuccess)
-    }
+        VStack {
+            ZStack {
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 300, height: 300)
+                    .onTapGesture {
+                        print("Rectangle tapped!")
+                    }
 
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.warning)
-    }
+                Circle()
+                    .fill(.red)
+                    .frame(width: 300, height: 300)
+                    .onTapGesture {
+                        print("Circle tapped!")
+                    }
+                    .allowsHitTesting(false)
+            }
 
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
-        }
-    }
-
-    func complexSuccess() {
-        // 장치가 햅틱을 지원하는지 확인
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-
-        // 강렬하고 날카로운 탭 하나 생성
-//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-//        events.append(event)
-
-        // 강도와 선명도를 증가시키다가 감소시키는 여러 탭 생성
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-
-        // 그 이벤트들을 패턴으로 변환하고 즉시 재생
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
+            VStack {
+                Text("Hello")
+                Spacer().frame(height: 100)
+                Text("World")
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                print("VStack tapped!")
+            }
         }
     }
 }
