95e31d9f55c7944a614c7f9314e3bf586535f937
Suho Park <psooh1219@icloud.com>
2023-09-13 21:24:19 +0900

    Day 87. [Flashzilla] 단일 CardView 디자인 하기

---
+++ project17/Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by 박수호 on 2023/09/13.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
--- project17/Flashzilla/Flashzilla/ContentView.swift
+++ project17/Flashzilla/Flashzilla/ContentView.swift
@@ -8,23 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
-
     var body: some View {
-        Text("Hello, World!")
-            .padding()
-            .background(reduceTransparency ? .black : .black.opacity(0.5))
-            .foregroundColor(.white)
-            .clipShape(Capsule())
-    }
-
-    // 모션 줄이기 옵션에 따라 자동 적용
-    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-        if UIAccessibility.isReduceMotionEnabled {
-            return try body()
-        } else {
-            return try withAnimation(animation, body)
-        }
+        CardView(card: Card.example)
     }
 }
 
