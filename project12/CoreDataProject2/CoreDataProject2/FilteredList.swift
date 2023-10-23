5f75c87871d8aecd0bec6bfa85c57998d844964e
Suho Park <psooh1219@icloud.com>
2023-10-23 21:57:51 +0900

    Day 58 Review. [CoreDataProject] Advanced - Dynamically filtering with any key and any value

--- project12/CoreDataProject2/CoreDataProject2/FilteredList.swift
+++ project12/CoreDataProject2/CoreDataProject2/FilteredList.swift
@@ -5,23 +5,31 @@
 //  Created by Suho Park on 10/23/23.
 //
 
+import CoreData
 import SwiftUI
 
-struct FilteredList: View {
-    @FetchRequest var fetchRequest: FetchedResults<Singer>
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    @FetchRequest var fetchRequest: FetchedResults<T>
+
+    // this is our content closure
+    // we'll call this once for each item in the list
+    let content: (T) -> Content
 
     var body: some View {
-        List(fetchRequest, id: \.self) { singer in
-            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        List(fetchRequest, id: \.self) { item in
+            self.content(item)
         }
     }
 
-    init(filter: String) {
-        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
     }
 }
 
 #Preview {
-    FilteredList(filter: "S")
-        .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    FilteredList(filterKey: "lastName", filterValue: "A") { (singer: Singer) in
+        Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+    }
+    .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
